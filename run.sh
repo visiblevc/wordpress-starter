35aeba1b2ff94c283260d02d5cebccf62b389d80
Louis-Rémi Babé <lrbabe@gmail.com>
2017-01-25 12:37:34 +0100

    relax fil permissions in /app when PHP_ENV is 'dev'

--- run.sh
+++ run.sh
@@ -119,8 +119,12 @@ main() {
 
   h3 "Adjusting file permissions"
   groupadd -f docker && usermod -aG docker www-data
-  find /app -type d ! -path "/app/vendor/*" ! -path "/app/.composer/*" -exec chmod 755 {} \;
-  find /app -type f ! -path "/app/vendor/*" ! -path "/app/.composer/*" -exec chmod 644 {} \;
+  if [[ "${PHP_ENV}" =~ ^dev ]]; then
+    chomd -R 777 /app
+  else
+    find /app -type d ! -path "/app/vendor/*" ! -path "/app/.composer/*" -exec chmod 755 {} \;
+    find /app -type f ! -path "/app/vendor/*" ! -path "/app/.composer/*" -exec chmod 644 {} \;
+  fi
   mkdir -p /app/wp-content/uploads
   chmod -R 775 /app/wp-content/uploads && \
     chown -R :docker /app/wp-content/uploads
