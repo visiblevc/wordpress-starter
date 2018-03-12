ba4f46be8c223a3c22717d6c241fd349ac4967cb
Derek P Sifford <dereksifford@gmail.com>
2018-03-12 15:39:27 -0400

    various improvements and build updates

---
+++ Dockerfile
@@ -0,0 +1,70 @@
+ARG PHP_VERSION=7.2
+FROM php:${PHP_VERSION}-apache
+ARG VERSION=latest
+LABEL maintainer="Derek P Sifford <dereksifford@gmail.com>" \
+      version="${VERSION}-php${PHP_VERSION}"
+
+# Install base requirements & sensible defaults + required PHP extensions
+# RUN echo "deb http://ftp.debian.org/debian $(sed -n 's/^VERSION=.*(\(.*\)).*/\1/p' /etc/os-release)-backports main" >> /etc/apt/sources.list \
+RUN apt-get update \
+    && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
+        bash-completion \
+        less \
+        libpng-dev \
+        libjpeg-dev \
+        libxml2-dev \
+        mariadb-client \
+        unzip \
+        sudo \
+        vim \
+        zip \
+    # && DEBIAN_FRONTEND=noninteractive apt-get -t stretch-backports install -y \
+    #     python-certbot-apache \
+    && rm -rf /var/lib/apt/lists/* \
+    && docker-php-ext-configure gd --with-png-dir=/usr --with-jpeg-dir=/usr \
+    && docker-php-ext-install \
+        exif \
+        gd \
+        mysqli \
+        opcache \
+        soap \
+        zip \
+    # See https://secure.php.net/manual/en/opcache.installation.php
+    && echo 'memory_limit = 512M' > /usr/local/etc/php/php.ini \
+    && { \
+        echo 'opcache.memory_consumption=128'; \
+        echo 'opcache.interned_strings_buffer=8'; \
+        echo 'opcache.max_accelerated_files=4000'; \
+        echo 'opcache.revalidate_freq=2'; \
+        echo 'opcache.fast_shutdown=1'; \
+        echo 'opcache.enable_cli=1'; \
+    } > /usr/local/etc/php/conf.d/opcache-recommended.ini \
+    && sed -i 's/AllowOverride None/AllowOverride All/g' /etc/apache2/apache2.conf \
+    # Fixes issue where error is logged stating apache could not resolve the
+    # fully qualified domain name
+    && echo 'ServerName localhost' > /etc/apache2/conf-available/fqdn.conf \
+    # Grab and install wp-cli from remote
+    && curl \
+        -o /usr/local/bin/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar \
+        -o /etc/bash_completion.d/wp-cli https://raw.githubusercontent.com/wp-cli/wp-cli/master/utils/wp-completion.bash \
+    && a2enconf fqdn \
+    && a2enmod rewrite expires \
+    && service apache2 restart
+
+# Install wp-cli, add scripts, create install directory & symlink
+COPY --chown=www-data:www-data run.sh /run.sh
+RUN useradd -ms /bin/bash -G www-data,sudo admin \
+    && echo "admin ALL=(root) NOPASSWD:ALL" > /etc/sudoers.d/admin \
+    && chmod 0440 /etc/sudoers.d/admin \
+    && chmod +x /usr/local/bin/wp /run.sh \
+    && mkdir -p /app \
+    && rm -fr /var/www/html \
+    && chown -R www-data:www-data /app /var/www \
+    && chmod g+rw /app /var/www \
+    && ln -s /app /var/www/html
+
+USER admin
+WORKDIR /app
+EXPOSE 80 443
+CMD ["/run.sh"]
+
--- run.sh
+++ run.sh
@@ -3,327 +3,247 @@
 
 # Environment
 # ------------
-ADMIN_EMAIL=${ADMIN_EMAIL:-"admin@${DB_NAME:-wordpress}.com"}
-DB_HOST=${DB_HOST:-db}
-DB_USER=${DB_USER:-root}
-DB_NAME=${DB_NAME:-wordpress}
-DB_PASS=${DB_PASS:-root}
-DB_PREFIX=${DB_PREFIX:-wp_}
-PERMALINKS=${PERMALINKS:-'/%year%/%monthnum%/%postname%/'}
-SERVER_NAME=${SERVER_NAME:-localhost}
-WP_VERSION=${WP_VERSION:-latest}
-URL_REPLACE=${URL_REPLACE:-''}
-BEFORE_URL="${URL_REPLACE%,*}"
-AFTER_URL="${URL_REPLACE#*,}"
+declare -x TERM="${TERM:-xterm}"
+declare ADMIN_EMAIL=${ADMIN_EMAIL:-"admin@${DB_NAME:-wordpress}.com"}
+declare AFTER_URL="${URL_REPLACE#*,}"
+declare BEFORE_URL="${URL_REPLACE%,*}"
+declare DB_HOST=${DB_HOST:-db}
+declare DB_NAME=${DB_NAME:-wordpress}
+declare DB_PASS=${DB_PASS:-root}
+declare DB_PREFIX=${DB_PREFIX:-wp_}
+declare DB_USER=${DB_USER:-root}
+declare PERMALINKS=${PERMALINKS:-'/%year%/%monthnum%/%postname%/'}
+declare PLUGINS="${PLUGINS//,/}"
+declare SERVER_NAME=${SERVER_NAME:-localhost}
+declare THEMES="${THEMES//,/}"
+declare URL_REPLACE=${URL_REPLACE:-''}
+declare WP_VERSION=${WP_VERSION:-latest}
 
 declare -A plugin_deps
 declare -A theme_deps
-declare -A plugin_volumes
-declare -A theme_volumes
 
-# Apache configuration
-# --------------------
-sed -i "s/#ServerName www.example.com/ServerName $SERVER_NAME\nServerAlias www.$SERVER_NAME/" /etc/apache2/sites-available/000-default.conf
+main() {
+    h1 'Begin WordPress Installation'
+    init
 
-# WP-CLI configuration
-# ---------------------
-cat > ~/.wp-cli/config.yml <<EOF
-apache_modules:
-  - mod_rewrite
+    h2 'Waiting for MySQL to initialize...'
+    while ! mysqladmin ping \
+        --host="$DB_HOST" \
+        --user="$DB_USER" \
+        --password="$DB_PASS" \
+        --silent >/dev/null; do
+        sleep 1
+    done
 
-core config:
-  dbuser: $DB_USER
-  dbpass: $DB_PASS
-  dbname: $DB_NAME
-  dbprefix: $DB_PREFIX
-  dbhost: $DB_HOST:3306
-  extra-php: |
-    define('WP_DEBUG', ${WP_DEBUG:-false});
-    define('WP_DEBUG_LOG', ${WP_DEBUG_LOG:-false});
-    define('WP_DEBUG_DISPLAY', ${WP_DEBUG_DISPLAY:-true});
+    h2 'Configuring WordPress'
+    configure
 
-core install:
-  url: ${AFTER_URL:-localhost:8080}
-  title: $DB_NAME
-  admin_user: $DB_USER
-  admin_password: $DB_PASS
-  admin_email: $ADMIN_EMAIL
-  skip-email: true
-EOF
+    h2 'Checking database'
+    check_database
 
-# WP-CLI bash completions
-# ------------------
-echo "
-. /etc/bash_completion.d/wp-cli
-" >> /root/.bashrc
-. /etc/bash_completion.d/wp-cli
+    if [[ "$MULTISITE" == 'true' ]]; then
+        h2 'Enabling Multisite'
+        wp --color core multisite-convert |& logger
+    fi
 
-# Helpers
-# ---------------------
+    h2 'Checking themes'
+    check_themes
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-PURPLE='\033[0;34m'
-CYAN='\033[0;36m'
-BOLD='\E[1m'
-NC='\033[0m'
+    h2 'Checking plugins'
+    check_plugins
 
-h1() {
-  local len=$(($(tput cols)-1))
-  local input=$*
-  local size=$(((len - ${#input})/2))
+    h2 'Finalizing'
+    if [[ "$MULTISITE" != 'true' ]]; then
+        wp --color rewrite structure "$PERMALINKS" --hard |& logger
+    fi
 
-  for ((i = 0; i < len; i++)); do echo -ne "${PURPLE}${BOLD}="; done; echo ""
-  for ((i = 0; i < size; i++)); do echo -n " "; done; echo -e "${NC}${BOLD}$input"
-  for ((i = 0; i < len; i++)); do echo -ne "${PURPLE}${BOLD}="; done; echo -e "${NC}"
-}
+    h1 'WordPress Configuration Complete!'
 
-h2() {
-  echo -e "${ORANGE}${BOLD}==>${NC}${BOLD} $*${NC}"
-}
-
-
-_colorize() {
-  local IN
-  local success="${GREEN}${BOLD}Success:${NC}"
-  local failed="${RED}${BOLD}Error:${NC}"
-  local warning="${CYAN}${BOLD}Warning:${NC}"
-  while read -r IN; do
-    IN="${IN/Success\:/$success}"
-    IN="${IN/Error\:/$failed}"
-    IN="${IN/Warning\:/$warning}"
-    echo -e "$IN"
-  done
-}
-
-_log_last_exit_colorize() {
-  if [ $? -eq 0 ]; then
-    echo "$1" |& _colorize
-  else
-    echo "$2" |& _colorize
-    exit 1
-  fi
-}
-
-_get_volumes() {
-  local volume_type="$1"
-  local filenames dirnames
-  local names=()
-
-  filenames=$(
-    find /app/wp-content/"$volume_type"/* -maxdepth 0 -type f ! -name 'index*' -print0 2>/dev/null |
-    xargs -0 -I {} basename {} .php
-  )
-  dirnames=$(
-    find /app/wp-content/"$volume_type"/* -maxdepth 0 -type d -print0 2>/dev/null |
-    xargs -0 basename -a 2>/dev/null
-  )
-  names=( $filenames $dirnames )
-
-  echo "${names[@]}"
-}
-
-_wp() {
-  wp --allow-root "$@"
+    sudo rm -f /var/run/apache2/apache2.pid
+    sudo apache2-foreground
 }
 
 # Config Functions
 # ---------------------
 
 init() {
-  local plugins themes i
+    declare raw_line
+    declare -a keyvalue
 
-  PLUGINS="${PLUGINS/%,},"
-  THEMES="${THEMES/%,},"
+    for raw_line in $PLUGINS; do
+        mapfile -d$'\n' -t keyvalue < <(
+            sed -n '
+                s/.*\[\(.*\)\]\([^[:blank:]]*\).*/\1\n\2/p # Matches [key]value form
+                t                                          # If previous match succeeds, skip to end
+                {p; p;}                                    # Assumes normal form
+            ' <<<"$raw_line")
+        plugin_deps[${keyvalue[0]}]="${keyvalue[1]}"
+    done
 
-  if [[ -f /root/.dockercache ]]; then
-    . /root/.dockercache
-  else
-    plugins=$( _get_volumes plugins )
-    themes=$( _get_volumes themes )
-    echo "plugins='$plugins'" >> ~/.dockercache
-    echo "themes='$themes'" >> ~/.dockercache
-  fi
+    for raw_line in ${THEMES:=twentyseventeen}; do
+        mapfile -d$'\n' -t keyvalue < <(
+            sed -n '
+                s/.*\[\(.*\)\]\([^[:blank:]]*\).*/\1\n\2/p # Matches [key]value form
+                t                                          # If previous match succeeds, skip to end
+                {p; p;}                                    # Assumes normal form
+            ' <<<"$raw_line")
+        theme_deps[${keyvalue[0]}]="${keyvalue[1]}"
+    done
 
-  for i in $plugins; do
-    plugin_volumes[$i]="$i"
-  done
-
-  for i in $themes; do
-    theme_volumes[$i]="$i"
-  done
-
-  local key value IFS=$'\n'
-  while read -r -d, i; do
-    [[ ! "$i" ]] && continue
-    i="${i# }"          # Trim leading whitespace
-    key="${i%]*}"       # Trim right bracket to end of string
-    key="${key//[\[ ]}" # Trim left bracket
-    value="${i##\[*\]}" # Trim bracketed text inclusive
-    plugin_deps[$key]="$value"
-  done <<< "$PLUGINS"
-
-  while read -r -d, i; do
-    [[ ! "$i" ]] && continue
-    i="${i# }"          # Trim leading whitespace
-    key="${i%]*}"       # Trim right bracket to end of string
-    key="${key//[\[ ]}" # Trim left bracket
-    value="${i##\[*\]}" # Trim bracketed text inclusive
-    theme_deps[$key]="$value"
-  done <<< "$THEMES"
-
-  # Download WordPress
-  # ------------------
-  if [[ ! -f /app/wp-settings.php ]]; then
-    h2 "Downloading WordPress"
-    _wp core download --version="$WP_VERSION"
-    _log_last_exit_colorize "Success: Wordpress downloaded" "Error: Wordpress download failed!"
-  fi
-
-  chown -R www-data /app/wp-content
+    if [[ ! -f /app/wp-settings.php ]]; then
+        h2 'Downloading WordPress'
+        wp --color core download --version="$WP_VERSION" --skip-content |& logger
+    fi
 }
 
 check_database() {
-  local data_path
+    wp core is-installed 2>/dev/null && return
 
-  # Already installed
-  wp core is-installed --allow-root 2>/dev/null && return
+    wp --color db create |& logger
 
-  _wp db create
-  _log_last_exit_colorize "Success: db create" "Error: db create failed!"
+    declare data_path
+    data_path=$(find /data -name '*.sql' -print -quit 2>/dev/null)
+    if [[ ! "$data_path" ]]; then
+        wp --color core install |& logger
+        return
+    fi
 
-  # No backups found
-  if [[ "$( find /data -name "*.sql" 2>/dev/null | wc -l )" -eq 0 ]]; then
-    _wp core install
-    _log_last_exit_colorize "Success: core install" "Error: core install failed!"
+    wp --color db import "$data_path" |& logger
 
-    return
-  fi
-
-  data_path=$( find /data -name "*.sql" -print -quit )
-  _wp db import "$data_path"
-  _log_last_exit_colorize "Success: db import" "Error: db import failed!"
-
-  if [[ -n "$URL_REPLACE" ]]; then
-    wp search-replace --skip-columns=guid "$BEFORE_URL" "$AFTER_URL" --allow-root \
-    | grep 'replacement' \
-    |& _colorize
-  fi
+    if [[ -n "$URL_REPLACE" ]]; then
+        wp --color search-replace --skip-columns=guid "$BEFORE_URL" "$AFTER_URL" \
+            | grep --color 'replacement' \
+            |& logger
+    fi
 }
 
+# Install / remove plugins based on $PLUGINS in parallel threads
 check_plugins() {
-  local key
-  local plugin
-  local to_install=()
-  local to_remove=()
+    (
+        declare -a add_list
+        mapfile -t add_list < <(comm -23 \
+            <(echo "${!plugin_deps[@]}" | tr ' ' '\n' | sort) \
+            <(wp plugin list --field=name | sort))
 
-  if [[ "${#plugin_deps[@]}" -gt 0 ]]; then
-    for key in "${!plugin_deps[@]}"; do
-      if ! wp plugin is-installed --allow-root "$key"; then
-        to_install+=( "${plugin_deps[$key]}" )
-      fi
-    done
-  fi
+        if [[ "${#add_list[@]}" -gt 0 ]]; then
+            wp --color plugin install --activate "${add_list[@]}" |& logger
+        fi
+    ) &
 
-  for plugin in $(wp plugin list --field=name --allow-root); do
-    [[ ${plugin_deps[$plugin]} ]] && continue
-    [[ ${plugin_volumes[$plugin]} ]] && continue
-    to_remove+=( "$plugin" )
-  done
+    (
+        declare -a remove_list
+        mapfile -t remove_list < <(comm -13 \
+            <(echo "${!plugin_deps[@]}" | tr ' ' '\n' | sort) \
+            <(wp plugin list --field=name | sort))
 
-  for key in "${to_install[@]}"; do
-    wp plugin install --allow-root "$key"
-    _log_last_exit_colorize "Success: $key plugin installed" "Error: $key plugin install failure!"
-  done
+        if [[ ${#remove_list[@]} -gt 0 ]]; then
+            wp --color plugin uninstall --deactivate "${remove_list[@]}" |& logger
+        fi
+    ) &
 
-  [[ "${#to_remove}" -gt 0 ]] && _wp plugin delete "${to_remove[@]}"
-  _wp plugin activate --all
-  _log_last_exit_colorize "Success: plugin activate all" "Error: plugin activate all failed!"
- }
-
-check_themes() {
-  local key
-  local theme
-  local to_install=()
-  local to_remove=()
-
-  if [[ "${#theme_deps[@]}" -gt 0 ]]; then
-    for key in "${!theme_deps[@]}"; do
-      if ! wp theme is-installed --allow-root "$key"; then
-        to_install+=( "${theme_deps[$key]}" )
-      fi
-    done
-  fi
-
-  for key in "${to_install[@]}"; do
-    wp theme install --allow-root "$key"
-    _log_last_exit_colorize "Success: $key theme install " "Error: $key theme install failed!"
-  done
-
-  for theme in $(wp theme list --field=name --status=inactive --allow-root); do
-    [[ ${theme_deps[$theme]} ]] && continue
-    [[ ${theme_volumes[$theme]} ]] && continue
-    to_remove+=( "$theme" )
-  done
-
-  for key in "${to_remove[@]}"; do
-    wp theme delete --allow-root "$key"
-    _log_last_exit_colorize "Success: $key theme deleted" "Error: $key theme delete failed!"
-  done
-
+    wait
 }
 
-main() {
-  h1 "Begin WordPress Installation"
-  init
+# Install / remove themes based on $THEMES in parallel threads
+check_themes() {
+    (
+        declare -a add_list
+        mapfile -t add_list < <(comm -23 \
+            <(echo "${!theme_deps[@]}" | tr ' ' '\n' | sort) \
+            <(wp theme list --field=name | sort))
 
-  # Wait for MySQL
-  # --------------
-  h2 "Waiting for MySQL to initialize..."
-  while ! mysqladmin ping --host="$DB_HOST" --password="$DB_PASS" --silent; do
-    sleep 1
-  done
+        if [[ "${#add_list[@]}" -gt 0 ]]; then
+            wp --color theme install "${add_list[@]}" |& logger
+        fi
+    ) &
 
-  h2 "Configuring WordPress"
-  rm -f /app/wp-config.php
-  _wp core config
-  _log_last_exit_colorize "Success: core config" "Error: core config failed!"
+    (
+        declare -a remove_list
+        mapfile -t remove_list < <(comm -13 \
+            <(echo "${!theme_deps[@]}" | tr ' ' '\n' | sort) \
+            <(wp theme list --field=name | sort))
 
-  h2 "Checking database"
-  check_database
+        if [[ ${#remove_list[@]} -gt 0 ]]; then
+            wp --color theme delete "${remove_list[@]}" |& logger
+        fi
+    ) &
 
-  if [[ "$MULTISITE" == "true" ]]; then
-    h2 "Enabling Multisite"
-    _wp core multisite-convert
-    _log_last_exit_colorize "Success: core multisite-convert" "Error: core multisite-convert failed!"
-  fi
+    wait
+}
 
-  h2 "Checking themes"
-  check_themes
+configure() {
+    # Ensures that this only runs on the initial build
+    [[ -f ~/.wp-cli/config.yml ]] && return
 
-  h2 "Checking plugins"
-  check_plugins
+    # Apache config adustments
+    sudo sed -i \
+        -e "/^[[:blank:]]*.ServerName/{c\\" \
+        -e "\\tServerName ${SERVER_NAME} \\" \
+        -e "\\tServerAlias www.${SERVER_NAME}" \
+        -e '}' /etc/apache2/sites-available/000-default.conf
 
-  h2 "Finalizing"
-  if [[ "$MULTISITE" != 'true' ]]; then
-    _wp rewrite structure "$PERMALINKS"
-    _log_last_exit_colorize "Success: rewrite structure" "Error: rewrite structure failed!"
+    # Source bash completion now and on login
+    echo '. /etc/bash_completion.d/wp-cli' >>~/.bashrc
+    . /etc/bash_completion.d/wp-cli
 
-    _wp rewrite flush --hard
-    _log_last_exit_colorize "Success: rewrite flush" "Error: rewrite flush failed!"
-  fi
+    # WP-CLI defaults
+    mkdir -p ~/.wp-cli
+    cat <<EOF >~/.wp-cli/config.yml
+apache_modules:
+    - mod_rewrite
 
-  chown -R www-data /app /var/www/html
-  find /app -type d -exec chmod 755 {} \;
-  find /app -type f -exec chmod 644 {} \;
-  find /app \( -type f -or -type d \) ! -group root -exec chmod g+rw {} \;
+core config:
+    dbuser: $DB_USER
+    dbpass: $DB_PASS
+    dbname: $DB_NAME
+    dbprefix: $DB_PREFIX
+    dbhost: $DB_HOST:3306
+    extra-php: |
+        define('WP_DEBUG', ${WP_DEBUG:-false});
+        define('WP_DEBUG_LOG', ${WP_DEBUG_LOG:-false});
+        define('WP_DEBUG_DISPLAY', ${WP_DEBUG_DISPLAY:-true});
 
-  h1 "WordPress Configuration Complete!"
+core install:
+    url: ${AFTER_URL:-localhost:8080}
+    title: $DB_NAME
+    admin_user: $DB_USER
+    admin_password: $DB_PASS
+    admin_email: $ADMIN_EMAIL
+    skip-email: true
+EOF
+    
+    # Create wp-config.php file
+    wp --color config create --force |& logger
+}
 
-  rm -f /var/run/apache2/apache2.pid
-  . /etc/apache2/envvars
-  exec apache2 -D FOREGROUND
+# Helpers
+# ---------------------
+
+declare -i term_width=70
+
+h1() {
+    declare border padding text
+    border='\e[1;34m'"$(printf '=%.0s' $(seq 1 "$term_width"))"'\e[0m'
+    padding="$(printf ' %.0s' $(seq 1 $(((term_width - $(wc -m <<<"$*")) / 2))))"
+    text="\\e[1m$*\\e[0m"
+    echo -e "$border"
+    echo -e "${padding}${text}${padding}"
+    echo -e "$border"
+}
+
+h2() {
+    printf '\e[1;33m==>\e[37;1m %s\e[0m\n' "$*"
+}
+
+logger() {
+    fold --width $((term_width - 9)) -s | sed -n '
+    /^\x1b\[[0-9;]*m/{ # match any line beginning with colorized text
+        s/Error:/  \0/ # pads line so its length matches others
+        p              # any lines containing color
+        b              # branch to end
+    }
+    s/.*/         \0/p # pads all other lines with 9 spaces
+    '
 }
 
 main
