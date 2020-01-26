0be8ea9b76dc1bf54dd56946040673d3c3b74ae2
jiverson <jiverson222@gmail.com>
2020-01-26 11:16:24 -0700

    fix: theme install folder

--- run.sh
+++ run.sh
@@ -160,7 +160,8 @@ init() {
     done
 
     # If no theme dependencies or volumes exist, fall back to default
-    if [[ ${#theme_deps[@]} == 0 && -d /app/wp-content/themes && $(find /app/wp-content/themes/* -maxdepth 0 -type d | wc -l) == 0 ]]; then
+    if [[ (${#theme_deps[@]} == 0 && ! -d /app/wp-content/themes) ||
+            (-d /app/wp-content/themes && $(find /app/wp-content/themes/* -maxdepth 0 -type d | wc -l) == 0)]]; then
         theme_deps["$default_theme"]="$default_theme"
     fi
 
